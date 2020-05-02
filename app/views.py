2969ba36772ce00841ea982c6b2be0dfe2d8a167
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-05-02 09:44:44 +0300

    setup of the configurations

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = "Home - Post a picth and review others!"
+    return render_template('index.html',title = title)
+
+@app.route('/profile/<user>')
+def profile(user):
+
+    '''
+    View profile page function that returns the profile page and its pitches
+    '''
+    return render_template('profile.html',id = user)
